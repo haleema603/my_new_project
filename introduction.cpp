a4499caeb1bf0795aa599fcc858e67d63f532419
haleema603 <haleemaabid586@gmail.com>
2024-09-27 21:26:33 +0500

    new code

---
+++ introduction.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"My name is Haleema Abid\n";
+	cout<<"I am from Abbottabad\n";
+	cout<<"I have completed my ICS from FIC Minhas\n";
+	cout<<"I have joined this program to learn cybersecurity skills.";
+	return 0;
+}
